584b53d3cac9789041c13942aa21a2677e209c0a
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2026-01-01 22:07:33 +0530

    chore: initialize http-server package with express dependency

--- js program/forOffLoops.js
+++ js program/forOffLoops.js
@@ -1,4 +1,17 @@
-let cars = ["Tata", "Mahindra", "Tesla"]
-for(let car of cars){
-    console.log(car)
+const { log } = require("console");
+
+let cars = ["Tata", "Mahindra", "Tesla"];
+for (let car of cars) {
+  console.log(car);
+}
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) {
+  console.log(num);
+}
+
+const greetings = "Hello World";
+for (const greet of greetings) {
+  console.log(`Each char is ${greet}`);
 }
